bdd87bb4752fb33cd6ef539771a4e10562e41ed7
pkrysiak <pawelkrysiak90@gmail.com>
2015-02-18 01:59:27 +0000

    created API in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
 
   def index
-    @animals = Animal.all
+    animals = Animal.all
+    respond_to do |format|
+      format.html {
+        animals
+      }
+      format.json {
+        render json: animals
+      }
+    end
   end
 
 end
